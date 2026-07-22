24cfff29873e6ee7dfbadd08fba4abf31b8c8523
bnc_nermine <nerminebennaci5@gmail.com>
2026-07-22 17:45:36 +0200

    data base config

---
+++ src/lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXoXrOkdpZ7w9LV-0Rryi22FNJaQYL-ZM',
+  authDomain: 'firstloc-71a47.firebaseapp.com',
+  projectId: 'firstloc-71a47',
+  storageBucket: 'firstloc-71a47.firebasestorage.app',
+  messagingSenderId: '929494398127',
+  appId: '1:929494398127:web:7570b1077e9d14f519505a'
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
